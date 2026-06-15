bbf7c190c6da0e07ff14fb9b4496ab6fb9a15d8f
Salih Derin <derin.salih@gmx.de>
2026-06-15 20:07:57 +0200

    PokemontypeColor

--- script/template.js
+++ script/template.js
@@ -5,7 +5,7 @@ function getPokemonCardTemplate(pokemon) {
             <p>#${pokemon.id}</p>
             <h2>${pokemon.name}</h2>
         </div>
-        <div class="pokemon-card-img">
+        <div class="pokemon-card-img" style="background-color: ${pokemonTypeColors[pokemon.types[0].type.name]}">
             <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
         </div>
         <div class="pokemon-card-type">
