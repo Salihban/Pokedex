31564485ad08234ec7da767b5ff21a56a2c31c2d
Salih Derin <derin.salih@gmx.de>
2026-06-18 11:01:44 +0200

    search

--- script/search.js
+++ script/search.js
@@ -1,6 +1,12 @@
 function searchPokemon() {
     let search = document.getElementById("search-input").value.toLowerCase();
 
+    if (search.length < 3) {
+        alert("Please enter 3 letters");
+    } else {
+        
+    }
+
     currentPokemon = allPokemon.filter(pokemon => pokemon.name.includes(search));
     renderPokemon();
 }
